83e00f4fa58e769c45d82b64d25583f8bb5d5d1b
Vitor Markis <vitormarkis2369@gmail.com>
2023-02-19 21:05:59 -0300

    initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+// import { app } from "./app"
+
+// app.get("/", (req: Request, res: Response) => {
+//   return res.json({ message: "X da ooooooooo" })
+// })
+
+// app.listen(3333, () => console.log("Server is running on port 3333"))
+
+const vitor = "vitorLAssdfsdfsdf"
+const djfs = "ffffffffffff"
+const vitofkd = "dfdsfdsfdsf"
+const dfjsjr = "aaaa"
+
+console.log(vitor)
